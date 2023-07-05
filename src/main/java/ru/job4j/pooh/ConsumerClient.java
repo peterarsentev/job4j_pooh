463f0a9a2e07d18cb4360fa28f08a63b27594566
parsentev <parsentev@yandex.ru>
2023-07-05 15:01:02 +0300

    Change name of consumer.

--- src/main/java/ru/job4j/pooh/ConsumerClient.java
+++ src/main/java/ru/job4j/pooh/ConsumerClient.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class SlowConsumerClient {
+public class ConsumerClient {
     public static void main(String[] args) throws Exception {
         var socket = new Socket("127.0.0.1", 9000);
         try (var out = new PrintWriter(socket.getOutputStream());
